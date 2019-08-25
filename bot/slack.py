697baf5265b62511a84fd5d24466bcc751889b23
Derrick Walton <derrick.e.walton@gmail.com>
2019-08-25 10:06:09 -0600

    more cleanup

--- bot/slack.py
+++ bot/slack.py
@@ -13,18 +13,16 @@ from commands.unban_all import unban_all
 from commands.doc import doc_command
 from commands.help import create_commands_table
 from commands.score import get_karma, top_karma
-from commands.tip import get_random_tip
-from commands.welcome import welcome_user
-from commands.zen import import_this
 
+# TODO: Migrate a good portion of this to a config file.
+# this information should be dynamically loaded if was to
+# become a portable solution. 
 Message = namedtuple('Message', 'giverid channel text')
-GENERAL_CHANNEL = 'C4SFQJJ9Z'
+GENERAL_CHANNEL = 'CELQ0PWAC'  # Channel ID from our slack
 ADMINS = ('UEMN5QPLM')  # I'm the admin now.
-TEXT_FILTER_REPLIES = dict(zen='`import this`',
-                           cheers=':beers:',
+TEXT_FILTER_REPLIES = dict(cheers=':beers:',
                            braces='`SyntaxError: not a chance`')
 
-AUTOMATED_COMMANDS = dict(welcome=welcome_user)  # not manual
 ADMIN_BOT_COMMANDS = dict(top_karma=top_karma,
                           ban=ban_user,
                           unban=unban_user,
@@ -32,14 +30,14 @@ ADMIN_BOT_COMMANDS = dict(top_karma=top_karma,
                           )
 PUBLIC_BOT_COMMANDS = dict(age=pybites_age,
                            add=add_command,
-                           help=create_commands_table,
-                           tip=get_random_tip)
+                           help=create_commands_table
+                           )
 PRIVATE_BOT_COMMANDS = dict(
                             doc=doc_command,
                             help=create_commands_table,  # have everywhere
-                            karma=get_karma,
+                            karma=get_karma
                             )
-
+# END TODO
 
 def create_help_msg(is_admin):
     bot = "@lord_commander"
@@ -81,22 +79,14 @@ def post_msg(channel_or_user, text):
 def _get_cmd(text, private=True):
     if private:
         return text.split()[0].strip().lower()
-
-    # bot command needs to have bot fist in msg
+    # TODO: Botname should be pulled from config
     if not text.strip('<>@').startswith((KARMA_BOT, 'lord_commander')):
         return None
-
-    # need at least a command after karmabot
     if text.strip().count(' ') < 1:
         return None
-
-    # @lord_commander blabla -> get blabla
     cmd = text.split()[1]
-
-    # of course ignore karma points
     if cmd.startswith(('+', '-')):
         return None
-
     return cmd.strip().lower()
 
 
@@ -105,26 +95,19 @@ def perform_bot_cmd(msg, private=True):
     user = msg.get('user')
     userid = user and user.strip('<>@')
     is_admin = userid and userid in ADMINS
-
     channel = msg.get('channel')
     text = msg.get('text')
-
     command_set = private and PRIVATE_BOT_COMMANDS or PUBLIC_BOT_COMMANDS
     cmd = text and _get_cmd(text, private=private)
-
     if not cmd:
         return None
-
     if cmd == 'help':
         return create_help_msg(is_admin)
-
     command = command_set.get(cmd)
     if private and is_admin and cmd in ADMIN_BOT_COMMANDS:
         command = ADMIN_BOT_COMMANDS.get(cmd)
-
     if not command:
         return None
-
     kwargs = dict(user=lookup_username(user),
                   channel=channel,
                   text=text)
@@ -137,10 +120,8 @@ def perform_text_replacements(text):
     strip_chars = '?!'
     matching_words = [word.strip(strip_chars) for word in words
                       if word.strip(strip_chars) in TEXT_FILTER_REPLIES]
-
     if not matching_words:
         return None
-
     match_word = matching_words[0]
     replace_word = TEXT_FILTER_REPLIES.get(match_word)
     return 'To _{}_ I say: {}'.format(match_word, replace_word)
@@ -155,33 +136,26 @@ def parse_next_msg():
     user = msg.get('user')
     channel = msg.get('channel')
     text = msg.get('text')
-
     type_event = msg.get('type')
     if type_event == 'channel_created':
         bot_joins_new_channel(msg)
         return None
-
-    # not sure but sometimes we get dicts?
+    # TODO: clean these values better OR discover why we get dicts and fix that. 
     if (not isinstance(channel, str) or
        not isinstance(user, str) or
        not isinstance(text, str)):
         return None
-
-    # ignore anything karma bot says!
+    # Karma bot cant issue its own commands, we are ignoring karmabot
     if user == KARMA_BOT:
         return None
-
     text_replace_output = text and perform_text_replacements(text)
     if text_replace_output:
         post_msg(channel, text_replace_output)
-
     private = channel and channel.startswith('D')
     cmd_output = perform_bot_cmd(msg, private)
     if cmd_output:
         post_msg(channel, cmd_output)
         return None
-
     if not channel or not text:
         return None
-
     return Message(giverid=user, channel=channel, text=text)
